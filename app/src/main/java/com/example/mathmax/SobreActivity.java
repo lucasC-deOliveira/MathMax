43c7e275d3e275054340e49e419cf67f7a1c76ed
Pedro Vinicius <pedrovdjesus@hotmail.com>
2022-05-08 19:26:29 -0300

    activity_dados/activity_dados_cadastrados/activity_sobre

---
+++ app/src/main/java/com/example/mathmax/SobreActivity.java
@@ -0,0 +1,46 @@
+package com.example.mathmax;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SobreActivity extends AppCompatActivity {
+
+    private Button actButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sobre);
+        actButton = findViewById(R.id.sobrebutton);
+
+
+        if(MainActivity.isAutheticated){
+            goToApp();
+            finish();
+        }
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        actButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainActivity.isAutheticated = true;
+                goToApp();
+            }
+        });
+    }
+
+    private void goToApp(){
+        Intent main = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(main);
+        finish();
+    }
+}
